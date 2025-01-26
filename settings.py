6f98f6b0a4156a069243fa92b5e1ef353238db12
root <root@LAPTOP-061QP9LI>
2025-01-26 19:44:01 +0530

    created settings with necessary constants and configurations

---
+++ settings.py
@@ -0,0 +1,58 @@
+import customtkinter as ctk
+from tkinter import filedialog, messagebox
+
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 550
+
+A4_SIZE = {"width": 8.27, "height": 11.69}
+
+DPI = 300
+
+PAGE_SIZE = {"width": int (A4_SIZE['width'] * DPI), "height": int (A4_SIZE['height'] * DPI)}
+
+MAX_WIDTH = 2280
+
+COLORS = ["black", "red", "navy"]
+
+STYLES = {"Antonio":{"name_font_path":"fonts/Antonio-Bold.ttf", 
+                     "name_size":500, 
+                     "number_font_path":"fonts/Antonio-Regular.ttf", 
+                     "number_size":2700, 
+                     "name_y_padding":10, 
+                     "number_y_padding":200}, 
+
+          "Antonio & GUSHELINK Outline":{"name_font_path":"fonts/Antonio-Bold.ttf", 
+                                         "name_size":500, 
+                                         "number_font_path":"fonts/gushelink.outline.ttf", 
+                                         "number_size":3300, 
+                                         "name_y_padding":10, 
+                                         "number_y_padding":200}, 
+
+          "Calibri bold & Antonio":{"name_font_path":"fonts/calibri-bold.ttf", 
+                                    "name_size":690, 
+                                    "number_font_path":"fonts/Antonio-Regular.ttf", 
+                                    "number_size":2700, 
+                                    "name_y_padding":70, 
+                                    "number_y_padding":200}, 
+
+          "Calibri bold & GUSHELINK Outline":{"name_font_path":"fonts/calibri-bold.ttf", 
+                                              "name_size":690, 
+                                              "number_font_path":"fonts/gushelink.outline.ttf", 
+                                              "number_size":3300, 
+                                              "name_y_padding":70, 
+                                              "number_y_padding":200}, 
+
+          "Swis721 Blk BT Cond & Antonio":{"name_font_path":"fonts/Swiss721BT-BlackCondensed.ttf",
+                                            "name_size":610, 
+                                            "number_font_path":"fonts/Antonio-Regular.ttf", 
+                                            "number_size":2700, 
+                                            "name_y_padding":100, 
+                                            "number_y_padding":200}, 
+                                            
+          "Swis721 Blk BT Cond & GUSHELINK Outline":{"name_font_path":"fonts/Swiss721BT-BlackCondensed.ttf", 
+                                                     "name_size":610, 
+                                                     "number_font_path":"fonts/gushelink.outline.ttf", 
+                                                     "number_size":3300, 
+                                                     "name_y_padding":100, 
+                                                     "number_y_padding":200}}
