d043a112d922f90d5b3ec2c2e38e8fdf705808bb
root <root@LAPTOP-061QP9LI>
2025-02-06 21:05:22 +0530

    redefined folder path

--- settings.py
+++ settings.py
@@ -7,6 +7,10 @@ from PIL import Image, ImageTk, ImageDraw, ImageFont
 import pandas as pd
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STYLES_FOLDER = os.path.join(BASE_DIR, "styles")
+FONTS_FOLDER = os.path.join(BASE_DIR, "fonts")
+
 WINDOW_WIDTH = 650
 WINDOW_HEIGHT = 650
 
@@ -20,44 +24,44 @@ MAX_WIDTH = 2280
 
 COLORS = ["black", "red", "navy"]
 
-STYLES = {"Antonio":{"name_font_path":"fonts/Antonio-Bold.ttf", 
+STYLES = {"Antonio":{"name_font_path":f"{FONTS_FOLDER}/Antonio-Bold.ttf", 
                      "name_size":500, 
-                     "number_font_path":"fonts/Antonio-Regular.ttf", 
+                     "number_font_path":f"{FONTS_FOLDER}/Antonio-Regular.ttf", 
                      "number_size":2700, 
                      "name_y_padding":10, 
                      "number_y_padding":200}, 
 
-          "Antonio & GUSHELINK Outline":{"name_font_path":"fonts/Antonio-Bold.ttf", 
+          "Antonio & GUSHELINK Outline":{"name_font_path":f"{FONTS_FOLDER}/Antonio-Bold.ttf", 
                                          "name_size":500, 
-                                         "number_font_path":"fonts/gushelink.outline.ttf", 
+                                         "number_font_path":f"{FONTS_FOLDER}/gushelink.outline.ttf", 
                                          "number_size":3300, 
                                          "name_y_padding":10, 
                                          "number_y_padding":200}, 
 
-          "Calibri bold & Antonio":{"name_font_path":"fonts/calibri-bold.ttf", 
+          "Calibri bold & Antonio":{"name_font_path":f"{FONTS_FOLDER}/calibri-bold.ttf", 
                                     "name_size":690, 
-                                    "number_font_path":"fonts/Antonio-Regular.ttf", 
+                                    "number_font_path":f"{FONTS_FOLDER}/Antonio-Regular.ttf", 
                                     "number_size":2700, 
                                     "name_y_padding":70, 
                                     "number_y_padding":200}, 
 
-          "Calibri bold & GUSHELINK Outline":{"name_font_path":"fonts/calibri-bold.ttf", 
+          "Calibri bold & GUSHELINK Outline":{"name_font_path":f"{FONTS_FOLDER}/calibri-bold.ttf", 
                                               "name_size":690, 
-                                              "number_font_path":"fonts/gushelink.outline.ttf", 
+                                              "number_font_path":f"{FONTS_FOLDER}/gushelink.outline.ttf", 
                                               "number_size":3300, 
                                               "name_y_padding":70, 
                                               "number_y_padding":200}, 
 
-          "Swis721 Blk BT Cond & Antonio":{"name_font_path":"fonts/Swiss721BT-BlackCondensed.ttf",
+          "Swis721 Blk BT Cond & Antonio":{"name_font_path":f"{FONTS_FOLDER}/Swiss721BT-BlackCondensed.ttf",
                                             "name_size":610, 
-                                            "number_font_path":"fonts/Antonio-Regular.ttf", 
+                                            "number_font_path":f"{FONTS_FOLDER}/Antonio-Regular.ttf", 
                                             "number_size":2700, 
                                             "name_y_padding":100, 
                                             "number_y_padding":200}, 
                                             
-          "Swis721 Blk BT Cond & GUSHELINK Outline":{"name_font_path":"fonts/Swiss721BT-BlackCondensed.ttf", 
+          "Swis721 Blk BT Cond & GUSHELINK Outline":{"name_font_path":f"{FONTS_FOLDER}/Swiss721BT-BlackCondensed.ttf", 
                                                      "name_size":610, 
-                                                     "number_font_path":"fonts/gushelink.outline.ttf", 
+                                                     "number_font_path":f"{FONTS_FOLDER}/gushelink.outline.ttf", 
                                                      "number_size":3300, 
                                                      "name_y_padding":100, 
                                                      "number_y_padding":200}}
